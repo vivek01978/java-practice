d0ebb51aae500bb6b852cbd0b1c9a8ff10071697
vivek012 <vivekmadheshiya25@gmail.com>
2026-09-14 23:41:41 +0530

    Day 6 pratice

--- Pattern.java
+++ Pattern.java
@@ -1,4 +1,4 @@
-class Question{
+class Pattern{
     public static void main (String args[]){
         // for(int i =1; i<=5 ; i++){
         //     for(int j = 1 ; j<=i ; j++){
@@ -101,11 +101,6 @@ class Question{
         // *   *
         // *****
        
-       
-       
-       
-       
-       
         // nested loop
         // for (int i=1;i<=4;i++){
         //     for(int j=1; j<=5;j++){
@@ -131,12 +126,19 @@ class Question{
         //     System.out.println();
         // }
 
-        for(int i =1 ; i<=4 ; i++){
-            for(int j = 1 ; j<=i; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        // for(int i =1 ; i<=4 ; i++){
+        //     for(int j = 1 ; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // *
+        // **
+        // ***
+        // ****
+
+
 
 
     }
--- sorting.java
+++ sorting.java
@@ -1,8 +1,26 @@
 class Sorting {
+    public static void printArray(int arr[]){
+        for(int i =0 ; i<arr.length ; i++){
+            System.out.print(arr[i]);
+        }
+    }
     public static void main(String args[]){
-        StringBuilder name = new StringBuilder("vivek");
-        name.length();
-        System.out.println(name);
+        int arr[] = {7,2,4,8,9,1,3};
+
+        for(int i = 0 ; i<arr.length-1;i++){
+            for(int j= 0 ; j<arr.length-i-1 ; j++){
+                if(arr[j] > arr[j+1]){
+
+                    int tem = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tem; 
+                }
+
+            }
+
+        }
+
+        printArray(arr);
 
     }
 }
