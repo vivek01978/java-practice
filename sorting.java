f1d2d57d9d6901b347ebcbcc09ae4f78d70f1199
vivek012 <vivekmadheshiya25@gmail.com>
2026-09-15 00:04:24 +0530

    Day 6 practice

--- sorting.java
+++ sorting.java
@@ -5,22 +5,36 @@ class Sorting {
         }
     }
     public static void main(String args[]){
-        int arr[] = {7,2,4,8,9,1,3};
+        // int arr[] = {7,2,4,8,9,1,3};
 
-        for(int i = 0 ; i<arr.length-1;i++){
-            for(int j= 0 ; j<arr.length-i-1 ; j++){
-                if(arr[j] > arr[j+1]){
+        // for(int i = 0 ; i<arr.length-1;i++){
+        //     for(int j= 0 ; j<arr.length-i-1 ; j++){
+        //         if(arr[j] > arr[j+1]){
 
-                    int tem = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = tem; 
-                }
+        //             int tem = arr[j];
+        //             arr[j] = arr[j+1];
+        //             arr[j+1] = tem; 
+        //         }
 
-            }
+        //     }
 
+        // }
+
+        // printArray(arr);
+    // }
+    int arr[] = {7,8,3,1,2};
+    for(int i =1 ; i<arr.length; i++){
+        int current = arr[i];
+        int j = i-1;
+        while(j>= 0 && current<arr[j]){
+            arr[j+1] = arr[j];
+            j--;
         }
+        arr[j+1] = current;
 
-        printArray(arr);
+    }
+    printArray(arr);
+
 
     }
 }
